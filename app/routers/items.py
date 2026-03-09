e29319943fd49f3c512e1c5779d72b7b3e708f5e
Tyron Ingram <tyrnngrm@gmail.com>
2026-03-09 15:09:01 +0000

    Working MVP

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,6 +1,8 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 from ..database import get_db
-from ..schemas import MediaType, ItemStatus, ItemCreate
+from ..schemas import MediaType, ItemStatus, ItemCreate, ItemResponse, ItemUpdate
 from ..models import Item
 
 router = APIRouter(
@@ -8,22 +10,54 @@ router = APIRouter(
     tags=["items"]
 )
 
-@router.post("/")
-async def add_item():
-    pass
+@router.post("/", response_model=ItemResponse)
+async def add_item(item: ItemCreate, db: Session = Depends(get_db)):
+    new_item = Item(**item.model_dump(mode="json"))
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
 
-@router.get("/")
-async def get_items():
-    pass
+@router.get("/", response_model=List[ItemResponse])
+async def get_items(media_type: MediaType | None = None, status: ItemStatus | None = None, title: str | None = None, limit: int = 10, db: Session = Depends(get_db)):
+    query = db.query(Item)
 
-@router.get("/{item_id}")
-async def get_item():
-    pass
+    if media_type is not None:
+        query = query.filter(Item.media_type == media_type.value)
+    if status is not None:
+        query = query.filter(Item.status == status.value)
+    if title is not None:
+        query = query.filter(Item.title.ilike(f"%{title}%"))
 
-@router.patch("/{item_id}")
-async def update_item():
-    pass
+    return query.limit(limit).all()
+
+
+@router.get("/{item_id}", response_model=ItemResponse)
+async def get_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if not db_item:
+        raise HTTPException(status_code=status.HTTP_404_NO_CONTENT, detail="Item not found.")
+    return db_item
+
+@router.patch("/{item_id}", response_model=ItemResponse)
+async def update_item(item_id: int, item: ItemUpdate, db: Session = Depends(get_db)):
+
+    db_item  = db.query(Item).filter(Item.id == item_id).first()
+    if not db_item:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found.")
+    
+    for field, value in item.model_dump(mode="json").items():
+        setattr(db_item, field, value)
+
+    db.commit()
+    db.refresh(db_item)
+    return db_item
 
 @router.delete("/{item_id}")
-async def delete_item():
-    pass
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if not db_item:
+        raise HTTPException(status_code=status.HTTP_404_NO_CONTENT, detail="Item not found.")
+    db.delete(db_item)
+    db.commit()
+    return status.HTTP_204_NO_CONTENT
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 from enum import Enum
 from pydantic import BaseModel, Field
 from typing import Optional
@@ -25,3 +26,21 @@ class ItemCreate(BaseModel):
     notes: Optional[str] = None
     rating: Optional[float] = None
     priority: Optional[int] = None
+
+class ItemResponse(BaseModel):
+    id: int
+    title: str = Field(max_length=200)
+    media_type: str = Field(max_length=50)
+    status: str = Field(max_length=50)
+    notes: str | None = Field(default=None, max_length=500)
+    rating: float | None = None
+    priority: int | None = None
+    created_at: datetime
+
+class ItemUpdate(BaseModel):
+    title: str | None = Field(default=None, max_length=200)
+    media_type: MediaType | None = None
+    status: ItemStatus | None = None
+    notes: str | None = Field(default=None, max_length=500)
+    rating: float | None = None
+    priority: int | None = None
